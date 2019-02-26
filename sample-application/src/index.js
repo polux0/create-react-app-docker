45d15cd79182d74444450dffdba797c01f9c7833
Aleksa Stojanović <afidelo@protonmail.com>
2019-02-26 20:58:10 +0100

    started with `communicating with props`

--- sample-application/src/index.js
+++ sample-application/src/index.js
@@ -7,11 +7,20 @@ import * as serviceWorker from './serviceWorker';
 
 const App1 = () => {
 
-	return	<div>Hello boyz! Container related</div>;
+const buttonText = {text: 'click me!'};
+const labelText = 'Enter your name: ';
+
+	return	( 
+		<div>
+			<label className="label" htmlFor="name">{labelText}</label>
+			<input id="name" type="text" />
+			<button style={{backgroundColor:'blue', color: 'white'}}> {buttonText.text} </button>
+		</div>
+		);
 
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App1 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
